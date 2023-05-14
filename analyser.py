586c890ab900493cddc9ea9567e1c7a90e4f1b14
DaryaBelka <225284@student.uek.krakow.pl>
2023-05-14 14:00:41 +0200

    14.05.2023 DaryaBelka(home)

--- analyser.py
+++ analyser.py
@@ -2,40 +2,41 @@ import os
 import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
+
 print(*[filename.split(".")[0] for filename in os.listdir("./opinions")], sep="\n")
 
 product_code = input("Podaj kod produktu: ")
 
 opinions = pd.read_json(f"./opinions/{product_code}.json")
-#opinions.score = opinions.score.map(lambda x: float(x[:-2].replace(",",".")))
+# opinions.score = opinions.score.map(lambda x: float(x[:-2].replace(",",".")))
 opinions.score = opinions.score.map(lambda x: float(x.split("/")[0].replace(",",".")))
+
 # opinions_count = len(opinions.index)
 opinions_count = opinions.shape[0]
 # pros_count = sum([False if len(p)==0 else True for p in opinions.pros])
 # cons_count = sum([False if len(c)==0 else True for c in opinions.cons])
 # pros_count = opinions.pros.map(lambda p: False if len(p)==0 else True).sum()
-# cons_count = opinions.pros.map(lambda c: False if len(c)==0 else True).sum()
+# cons_count = opinions.cons.map(lambda c: False if len(c)==0 else True).sum()
 pros_count = opinions.pros.map(bool).sum()
 cons_count = opinions.cons.map(bool).sum()
-avg_score = opinions.score.mean() 
-
-print(f'''Dla produktu o kodzie {product_code} dostępnych jest {opinions_count} opinii.
-Dla {pros_count} opinii dostępna jest liczba zalet,
-a dla {cons_count} dostępna jest liczba wad.
+# avg_score = opinions.score.map(lambda x: float(x.split("/")[0].replace(",","."))).mean()
+avg_score = opinions.score.mean().round(2)
+print(f'''Dla produktu o kodzie {product_code} 
+dostępnych jest {opinions_count} opinii.
+Dla {pros_count} opinii dostępna jest lista zalet,
+a dla {cons_count} dostępna jest liczba wad. 
 Średnia ocena produktu to {avg_score}.''')
 
-# Histogram czestosci wystepowania poszczegolnych ocen
-score = opinions.score.value_counts().reindex(list(np.arange(0,5.5,0.5)), fill_value = 0 )
-print(score)
+# histogram częstości występowania poszczególnych ocen
+score = opinions.score.value_counts().reindex(list(np.arange(0,5.5,0.5)), fill_value = 0)
 score.plot.bar(color="hotpink")
-plt.xticks(rotation = 0)
+plt.xticks(rotation=0)
 plt.title("Histogram ocen")
 plt.xlabel("Liczba gwiazdek")
 plt.ylabel("Liczba opinii")
 for index, value in enumerate(score):
-    plt.text(index,value+0.5, str(value), ha="center")
-#plt.show()
-
+    plt.text(index, value+0.5, str(value), ha="center")
+# plt.show()
 try:
     os.mkdir("./plots")
 except FileExistsError:
@@ -43,8 +44,8 @@ except FileExistsError:
 plt.savefig(f"./plots/{product_code}_score.png")
 plt.close()
 
-#udzial poszegolnyvh
-recommendation = opinions["recommendation"].value_counts(dropna= False).sort_index()
+# udział poszczególnych rekomendacji w ogólnej liczbie opinii
+recommendation = opinions["recommendation"].value_counts(dropna = False).sort_index()
 print(recommendation)
 recommendation.plot.pie(
     label="", 
--- scraper.py
+++ scraper.py
@@ -1,8 +1,10 @@
 import requests
 import json
+import os
 from bs4 import BeautifulSoup
 
-def get_element(ancestor, selector = None, attribute = None, return_list = False):
+
+def get_element(ancestor, selector=None, attribute=None, return_list=False):
     try:
         if return_list:
             return [tag.text.strip() for tag in ancestor.select(selector)].copy()
@@ -14,26 +16,26 @@ def get_element(ancestor, selector = None, attribute = None, return_list = False
     except (AttributeError, TypeError):
         return None
 
+
 selectors = {
     "opinion_id": [None, "data-entry-id"],
     "author": ["span.user-post__author-name"],
     "recommendation": ["span.user-post__author-recomendation > em"],
     "score": ["span.user-post__score-count"],
     "purchased": ["div.review-pz"],
-    "published_at": ["span.user-post__published > time:nth-child(1)","datetime"],
-    "purchased_at": ["span.user-post__published > time:nth-child(2)","datetime"],
+    "published_at": ["span.user-post__published > time:nth-child(1)", "datetime"],
+    "purchased_at": ["span.user-post__published > time:nth-child(2)", "datetime"],
     "thumbs_up": ["button.vote-yes > span"],
     "thumbs_down": ["button.vote-no > span"],
     "content": ["div.user-post__text"],
-    "pros": ["div.review-feature__col:has(> div.review-feature__title--positives) > div.review-feature__item",None, True],
-    "cons": ["div.review-feature__col:has(> div.review-feature__title--negatives) > div.review-feature__item",None, True]
+    "pros": ["div.review-feature__col:has(> div.review-feature__title--positives) > div.review-feature__item", None, True],
+    "cons": ["div.review-feature__col:has(> div.review-feature__title--negatives) > div.review-feature__item", None, True]
 }
 
-# product_code = input("Podaj kod produktu: ")
-product_code = "96685108"
+product_code = input("Podaj kod produktu: ")
 all_opinions = []
 url = f"https://www.ceneo.pl/{product_code}#tab=reviews"
-while(url):
+while (url):
     print(url)
     response = requests.get(url)
     page = BeautifulSoup(response.text, 'html.parser')
@@ -41,14 +43,18 @@ while(url):
     for opinion in opinions:
         single_opinion = {}
         for key, value in selectors.items():
-            single_opinion[key] = get_element(opinion,*value)
+            single_opinion[key] = get_element(opinion, *value)
         all_opinions.append(single_opinion)
     try:
-        url = "https://www.ceneo.pl"+ get_element(page, "a.pagination__next", "href")
+        url = "https://www.ceneo.pl" + \
+            get_element(page, "a.pagination__next", "href")
     except TypeError:
         url = None
 
 print(len(all_opinions))
+try:
+    os.mkdir("./opinions")
+except FileExistsError:
+    pass
 with open(f"./opinions/{product_code}.json", "w", encoding="UTF-8") as jf:
-    json.dump(all_opinions, jf, indent=4,ensure_ascii=False)
-
+    json.dump(all_opinions, jf, indent=4, ensure_ascii=False)
